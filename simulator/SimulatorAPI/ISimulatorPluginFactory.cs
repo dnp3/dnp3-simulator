c90c9ae7f8cf6f8a776eb36656cfdf32a557ffb1
Adam Crain <jadamcrain@gmail.com>
2015-08-31 10:02:47 -0400

    hello world

---
+++ simulator/SimulatorAPI/ISimulatorPluginFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Automatak.Simulator.Commons;
+
+namespace Automatak.Simulator.API
+{
+    public interface ISimulatorPluginFactory
+    {
+        ISimulatorPlugin Create(ILog log);        
+    }
+}
